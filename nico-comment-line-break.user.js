66e7a0f1012dfcb442a4b196797c0fe22dcddcf0
KOTO28 <k.tonton128@gmail.com>
2026-01-03 21:36:05 +0900

    first commit

---
+++ nico-comment-line-break.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        nico-comment-line-break
+// @description Insert a newline in the Niconico Douga comment field with Shift + Enter.
+// @description:ja ニコニコ動画のコメント欄で Shift + Enter で改行できるようにします。
+// @version     1.0.0
+// @author      KOTO28
+// @namespace   https://github.com/KOTO28
+// @match       *://www.nicovideo.jp/watch/*
+// @grant       none
+// @homepageURL https://github.com/KOTO28/nico-comment-line-break/
+// @downloadURL https://raw.githubusercontent.com/KOTO28/nico-comment-line-break/main/nico-comment-line-break.user.js
+// ==/UserScript==
+
+(function () {
+  "use strict";
+})();
